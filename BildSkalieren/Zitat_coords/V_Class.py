d6b04527a5c8a07f51abb72b4393e8ccedd5dcdc
Regexose <boris.joens@gmail.com>
2021-02-08 12:22:26 +0100

    draw color vertex on vert_surface only
    use pic.width to create list of vertex.locations TODO es dauert sehr lange

--- BildSkalieren/Zitat_coords/V_Class.py
+++ BildSkalieren/Zitat_coords/V_Class.py
@@ -2,59 +2,72 @@ class Vertex:
     vert_surface = None
     xs, ys = [], [] # cordinates splitted
     vectors = []
-    def __init__(self, name, coords, v_col):
+    def __init__(self, name, w, h, coords, v_col):
         self.name = name
+        self.h = h
+        self.w = w
         self.coords = coords
         self.v_col = v_col
-        self.surface = createGraphics(width, height)
+        self.surface = createGraphics(w, h)
         self.build_vertex()
         self.angle = self.calculate_angle()
         self.locations, self.vertex_locs = self.vertex_locations()
+        print(len(self.locations))
 
     def build_vertex(self):
-        # fill vertex with v.col
-        with self.surface.beginDraw():
-            # self.surface.background(125, 122, 30)
-            self.surface.beginShape()
-            self.surface.fill(self.v_col)
-            for i in range(len(self.coords)):
+        # make vert_surface and draw colored vertex on it
+        for i in range(len(self.coords)):
                 x = self.coords[i][0]
                 y = self.coords[i][1]
                 self.xs.append(x)
                 self.ys.append(y)
                 vec = PVector(x,y)
                 self.vectors.append(vec)
-                self.surface.vertex(x, y)
-            self.surface.endShape(CLOSE)
         vert_width = max(self.xs) - min(self.xs)
         vert_height = max(self.ys) - min(self.ys)
         self.vert_surface = createGraphics(vert_width, vert_height)
-        
-    
+        with self.vert_surface.beginDraw():
+            self.vert_surface.fill(self.v_col)
+            self.vert_surface.beginShape()
+            for x, y  in zip(self.xs, self.ys):
+                x -= min(self.xs)
+                y -= min(self.ys)
+                if x > 0 or y > 0:
+                    print(" x {}   y {}".format(x, y))
+                self.vert_surface.vertex(x, y)
+            self.vert_surface.endShape(CLOSE)
+       
     def vertex_locations(self):
-        locs, small_locs = [], []
-        with self.surface.beginDraw():
-            self.surface.loadPixels()
-            for x in range(self.surface.width):
-                for y in range(self.surface.height):
-                    loc = x + y*width
-                    c = self.surface.pixels[loc]
+        # fill the array locs with corresponding img.pixels
+        locs, small_locs, not_found = [], [], []
+        with self.vert_surface.beginDraw(): #needed to load pixels
+            # self.vert_surface.background(150, 50)
+            self.vert_surface.loadPixels()
+            print('pixels len:   ', len(self.vert_surface.pixels))
+            for loc_x in range(self.vert_surface.width):
+                for loc_y in range(self.vert_surface.height):
+                    loc = loc_x + loc_y * self.vert_surface.width
+                    c = self.vert_surface.pixels[loc]
                     #if a pixel has the color v.col copy its location
                     if c == self.v_col:
+                        # recalculate location by adding the x and y offset (min(xs/ys)) and using the width of the image (self.w)
+                        print("found x {}  found y {}".format(loc_x, loc_y))
+                        loc = (loc_x + min(self.xs)) + (loc_y + min(self.ys)) * self.w 
                         locs.append(loc)
-                        x1 = x - min(self.xs)
-                        y1 = y - min(self.ys)
+                        x1 = loc_x - min(self.xs)
+                        y1 = loc_y - min(self.ys)
                         small_locs.append(x1 + y1 * self.vert_surface.width)
-        # print("small  {}  locs  {}".format(small_locs, locs))
+                    else:
+                        print("not found x {}  found y {}  color {} ".format(loc_x, loc_y, c))
+                        not_found.append(color(c))
+        print("notfound   {}  locs  {}".format(len(not_found), len(locs)))
         return locs, small_locs
     
     def calculate_angle(self) :
         self.direction = PVector.sub(self.vectors[1], self.vectors[0])
         # self.vector.normalize()
         ref_vector = PVector(10, 0)
-        print("heading:  ", self.direction.heading())
-        print("self vec {}  ref vect {}".format(self.direction, ref_vector))
+        # print("self vec {}  ref vect {}".format(self.direction, ref_vector))
         stroke(255,0,0)
         angle = PVector.angleBetween(self.direction, ref_vector)
-        print(angle)
         return angle
--- BildSkalieren/Zitat_coords/Zitat_coords.pyde
+++ BildSkalieren/Zitat_coords/Zitat_coords.pyde
@@ -3,26 +3,26 @@ from V_Class import Vertex as v
 key_color = 10
 vertexes = []
 coords = [(518, 118), (575, 313), (605, 306), (545, 113)]
+coords2 = [(2084, 479), (2314, 1253), (2424, 1219), (2181, 449)] # original size
 t = 0
         
 def setup():
     size(1500, 1000)
     global pic, v, loc_v, copy_surface, v_color
     pic = loadImage("DSC05212.JPG")
+    # print("pic dimensions  w  {}  h {}".format(pic.width, pic.height))
     v_color = color(0, 180, 12)
-    v = v('zitat1', coords, v_color)
+    v = v('zitat1', pic.width, pic.height, coords2, v_color)
     copy_surface = createGraphics(v.vert_surface.width, v.vert_surface.height)
     with copy_surface.beginDraw():
         copy_surface.noFill()
     frameRate(12)
 
-    
 def draw():
     global t, pic, v, copy_surface
-    image(pic, 0, 0, width, height)
-    line(518, 118, 518 + 57, 118 + 195)
-    if keyPressed :
-        copyPixels(v)
+    # image(pic, 0, 0, width, height)
+    image(v.vert_surface, 100, 100)
+    # line(518, 118, 518 + 57, 118 + 195)
     xoff = noise(t)
     yoff = noise(t)
     xoff = map(xoff, 0, 1, 0, mouseX)
@@ -35,15 +35,35 @@ def draw():
     image(copy_surface, 0, 0)
     popMatrix()
     t += 0.02
+    
+def keyPressed():
+    global v
+    if key == 'c':
+        copyPixels(v)
+    if key == 'r':
+        resizeSurf(v.vert_surface)
 
 def copyPixels(v):
     global pic, copy_surface
     with copy_surface.beginDraw():
         copy_surface.loadPixels()
-        loadPixels()
+        pic.loadPixels()
         for index, loc in enumerate(v.locations):
-            copy_surface.pixels[v.vertex_locs[index]] = pixels[loc]
+            # print('index {}   loc  {} '.format(index, loc))
+            copy_surface.pixels[v.vertex_locs[index]] = pic.pixels[loc]
         copy_surface.updatePixels()
+
+def resizeSurf(surface):
+    global copy_surface
+    print("surface width before", copy_surface.width)
+    temp_img = copy_surface.get()
+    temp_img.resize(temp_img.width *4, temp_img.height *4)
+    print("new width  : ", copy_surface.width)
+    copy_surface = createGraphics(temp_img.width, temp_img.height)
+    print("copy_surf width after", copy_surface.width)
+    with copy_surface.beginDraw():
+        copy_surface.image(temp_img, 0, 0)
+    
 
 def changePixels():
     global key_color
